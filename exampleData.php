0298ce45b415f72f5ab9dc69438a161fb616aa70
Gabor <gabor@neogrady.hu>
2021-09-25 09:01:15 +0200

    Refaktoring...

---
+++ exampleData.php
@@ -0,0 +1,277 @@
+<?php 
+
+$exampleData = [
+    [
+        'valasztott-szak' => [
+            'egyetem' => 'ELTE',
+            'kar' => 'IK',
+            'szak' => 'Programtervező informatikus',
+        ],
+        'erettsegi-eredmenyek' => [
+            [
+                'nev' => 'magyar nyelv és irodalom',
+                'tipus' => 'közép',
+                'eredmeny' => '70%',
+            ],
+            [
+                'nev' => 'történelem',
+                'tipus' => 'közép',
+                'eredmeny' => '80%',
+            ],
+            [
+                'nev' => 'matematika',
+                'tipus' => 'emelt',
+                'eredmeny' => '90%',
+            ],
+            [
+                'nev' => 'angol nyelv',
+                'tipus' => 'közép',
+                'eredmeny' => '94%',
+            ],
+            [
+                'nev' => 'informatika',
+                'tipus' => 'közép',
+                'eredmeny' => '95%',
+            ],
+        ],
+        'tobbletpontok' => [
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'B2',
+                'nyelv' => 'angol',
+            ],
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'C1',
+                'nyelv' => 'német',
+            ],
+        ]
+    ],
+    [
+        'valasztott-szak' => [
+            'egyetem' => 'ELTE',
+            'kar' => 'IK',
+            'szak' => 'Programtervező informatikus',
+        ],
+        'erettsegi-eredmenyek' => [
+            [
+                'nev' => 'magyar nyelv és irodalom',
+                'tipus' => 'közép',
+                'eredmeny' => '70%',
+            ],
+            [
+                'nev' => 'történelem',
+                'tipus' => 'közép',
+                'eredmeny' => '80%',
+            ],
+            [
+                'nev' => 'matematika',
+                'tipus' => 'emelt',
+                'eredmeny' => '90%',
+            ],
+            [
+                'nev' => 'angol nyelv',
+                'tipus' => 'közép',
+                'eredmeny' => '94%',
+            ],
+            [
+                'nev' => 'informatika',
+                'tipus' => 'közép',
+                'eredmeny' => '95%',
+            ],
+            [
+                'nev' => 'fizika',
+                'tipus' => 'közép',
+                'eredmeny' => '98%',
+            ],
+        ],
+        'tobbletpontok' => [
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'B2',
+                'nyelv' => 'angol',
+            ],
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'C1',
+                'nyelv' => 'német',
+            ],
+        ],
+    ],
+    [
+        'valasztott-szak' => [
+            'egyetem' => 'ELTE',
+            'kar' => 'IK',
+            'szak' => 'Programtervező informatikus',
+        ],
+        'erettsegi-eredmenyek' => [
+            [
+                'nev' => 'matematika',
+                'tipus' => 'emelt',
+                'eredmeny' => '90%',
+            ],
+            [
+                'nev' => 'angol nyelv',
+                'tipus' => 'közép',
+                'eredmeny' => '94%',
+            ],
+            [
+                'nev' => 'informatika',
+                'tipus' => 'közép',
+                'eredmeny' => '95%',
+            ],
+        ],
+        'tobbletpontok' => [
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'B2',
+                'nyelv' => 'angol',
+            ],
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'C1',
+                'nyelv' => 'német',
+            ],
+        ],
+    ],
+    [
+        'valasztott-szak' => [
+            'egyetem' => 'ELTE',
+            'kar' => 'IK',
+            'szak' => 'Programtervező informatikus',
+        ],
+        'erettsegi-eredmenyek' => [
+            [
+                'nev' => 'magyar nyelv és irodalom',
+                'tipus' => 'közép',
+                'eredmeny' => '15%',
+            ],
+            [
+                'nev' => 'történelem',
+                'tipus' => 'közép',
+                'eredmeny' => '80%',
+            ],
+            [
+                'nev' => 'matematika',
+                'tipus' => 'emelt',
+                'eredmeny' => '90%',
+            ],
+            [
+                'nev' => 'angol nyelv',
+                'tipus' => 'közép',
+                'eredmeny' => '94%',
+            ],
+            [
+                'nev' => 'informatika',
+                'tipus' => 'közép',
+                'eredmeny' => '95%',
+            ],
+        ],
+        'tobbletpontok' => [
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'B2',
+                'nyelv' => 'angol',
+            ],
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'C1',
+                'nyelv' => 'német',
+            ],
+        ],
+    ],
+    [
+        'valasztott-szak' => [
+            'egyetem' => 'ELTE',
+            'kar' => 'IK',
+            'szak' => 'Programtervező informatikus',
+        ],
+        'erettsegi-eredmenyek' => [
+            [
+                'nev' => 'magyar nyelv és irodalom',
+                'tipus' => 'közép',
+                'eredmeny' => '15%',
+            ],
+            [
+                'nev' => 'történelem',
+                'tipus' => 'közép',
+                'eredmeny' => '80%',
+            ],
+            [
+                'nev' => 'matematika',
+                'tipus' => 'emelt',
+                'eredmeny' => '90%',
+            ],
+            [
+                'nev' => 'angol nyelv',
+                'tipus' => 'közép',
+                'eredmeny' => '94%',
+            ],
+            [
+                'nev' => 'fedettpályás ki néz magasabbra',
+                'tipus' => 'közép',
+                'eredmeny' => '95%',
+            ],
+        ],
+        'tobbletpontok' => [
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'B2',
+                'nyelv' => 'angol',
+            ],
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'C1',
+                'nyelv' => 'német',
+            ],
+        ],
+    ],
+    [
+        'valasztott-szak' => [
+            'egyetem' => 'ELTE',
+            'kar' => 'IK',
+            'szak' => 'Programtervező informatikus',
+        ],
+        'erettsegi-eredmenyek' => [
+            [
+                'nev' => 'magyar nyelv és irodalom',
+                'tipus' => 'közép',
+                'eredmeny' => '15%',
+            ],
+            [
+                'nev' => 'történelem',
+                'tipus' => 'közép',
+                'eredmeny' => '80%',
+            ],
+            [
+                'nev' => 'matematika',
+                'tipus' => 'emelt',
+                'eredmeny' => '90%',
+            ],
+            [
+                'nev' => 'angol nyelv',
+                'tipus' => 'közép',
+                'eredmeny' => '94%',
+            ],
+            [
+                'nev' => 'fedettpályás ki néz magasabbra',
+                'tipus' => 'közép',
+                'eredmeny' => '95%',
+            ],
+        ],
+        'tobbletpontok' => [
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'B2',
+                'nyelv' => 'angol',
+            ],
+            [
+                'kategoria' => 'Nyelvvizsga',
+                'tipus' => 'C1',
+                'nyelv' => 'német',
+            ],
+        ],
+    ]
+];
+
+
